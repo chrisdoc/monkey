9af77c7a5cd5c1729912238444bdb50289ddd4d3
Christoph Kieslich <christoph.kieslich@reaktor.com>
2018-05-13 19:09:32 +0300

    add more operators and keywords to our lexer

--- src/monkey/token/token.go
+++ src/monkey/token/token.go
@@ -16,8 +16,15 @@ const (
 	INT   = "INT"   // Integer 123456
 
 	// OPERATORS
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
 
 	COMMA     = ","
 	SEMICOLON = ";"
@@ -27,14 +34,28 @@ const (
 	LBRACE = "{"
 	RBRACE = "}"
 
+	// EQUALITY
+	EQ     = "=="
+	NOT_EQ = "!="
+
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
