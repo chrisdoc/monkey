30244fc00dd7dcd3d322d9ce04f0a9e22d4c0ff9
Christoph Kieslich <c.kieslich@gmail.com>
2018-05-16 17:54:41 +0200

    Fail tests if the parsed program contains errors

--- src/monkey/parser/parser.go
+++ src/monkey/parser/parser.go
@@ -82,6 +82,7 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
 }
@@ -90,7 +91,7 @@ func (p *Parser) Errors() []string {
 	return p.errors
 }
 
-func (p *Parser) PeekError(t token.TokenType) {
+func (p *Parser) peekError(t token.TokenType) {
 	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
 	p.errors = append(p.errors, msg)
 }
