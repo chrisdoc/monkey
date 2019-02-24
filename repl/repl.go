9128543a95f109027e84354ad6b4db53ed3d4c32
Christoph Kieslich <c.kieslich@gmail.com>
2019-02-24 11:36:07 +0100

    migrate repl to use evaluator

--- repl/repl.go
+++ repl/repl.go
@@ -3,9 +3,11 @@ package repl
 import (
 	"bufio"
 	"fmt"
-	"github.com/chrisdoc/monkey/lexer"
-	"github.com/chrisdoc/monkey/token"
 	"io"
+
+	"github.com/chrisdoc/monkey/evaluator"
+	"github.com/chrisdoc/monkey/lexer"
+	"github.com/chrisdoc/monkey/parser"
 )
 
 const PROMPT = ">> "
@@ -20,8 +22,39 @@ func Start(in io.Reader, out io.Writer) {
 		}
 		line := scanner.Text()
 		l := lexer.New(line)
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParseErrors(out, p.Errors())
+			continue
 		}
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+	}
+}
+
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
+func printParseErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
